29e634fb46947b918b86e7eb93b540fe5e3c2ac3
Jason <jvongsana@hotmail.com>
2021-01-04 11:52:41 -0800

    calculate winner and moved state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,28 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from "./Square";
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-
-  const handlSquareClick = (position) => {
-
-    if (board[position]) {
-      return;
-    }
-
-    setBoard((prev) => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
-
-        return square;
-      })
-    });
-
-    setIsXNext((prev) => !prev )
-  };
+const Board = ({ board, handlSquareClick }) => {
+ 
 
   const renderSquare = (position) => {
     return (
